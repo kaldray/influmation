441dc3c4d65874e7ccbd9c1e18f3f2b21551b842
kaldray <jordray@protonmail.com>
2024-11-18 11:29:49 +0100

    style: center layout

--- inertia/src/components/Layout.tsx
+++ inertia/src/components/Layout.tsx
@@ -1,11 +1,12 @@
 import { type PropsWithChildren } from 'react'
 import { Navbar } from './Navbar'
+import { css } from '#style/css'
 
 export function AuthLayout({ children }: PropsWithChildren) {
   return (
     <>
       <Navbar />
-      {children}
+      <section className={css({ maxWidth: '80%', margin: 'auto' })}>{children}</section>
     </>
   )
 }
