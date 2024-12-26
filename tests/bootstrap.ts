023d6e198ce53c066e98a19950608bc6f262b2ef
kaldray <jordray@protonmail.com>
2024-12-26 23:17:07 +0100

    chore: add test package client and snapshot

--- tests/bootstrap.ts
+++ tests/bootstrap.ts
@@ -3,6 +3,8 @@ import app from '@adonisjs/core/services/app'
 import type { Config } from '@japa/runner/types'
 import { pluginAdonisJS } from '@japa/plugin-adonisjs'
 import testUtils from '@adonisjs/core/services/test_utils'
+import { apiClient } from '@japa/api-client'
+import { snapshot } from '@japa/snapshot'
 
 /**
  * This file is imported by the "bin/test.ts" entrypoint file
@@ -12,7 +14,7 @@ import testUtils from '@adonisjs/core/services/test_utils'
  * Configure Japa plugins in the plugins array.
  * Learn more - https://japa.dev/docs/runner-config#plugins-optional
  */
-export const plugins: Config['plugins'] = [assert(), pluginAdonisJS(app)]
+export const plugins: Config['plugins'] = [assert(), apiClient(), snapshot(), pluginAdonisJS(app)]
 
 /**
  * Configure lifecycle function to run before and after all the
@@ -32,6 +34,8 @@ export const runnerHooks: Required<Pick<Config, 'setup' | 'teardown'>> = {
  */
 export const configureSuite: Config['configureSuite'] = (suite) => {
   if (['browser', 'functional', 'e2e'].includes(suite.name)) {
-    return suite.setup(() => testUtils.httpServer().start())
+    return suite.setup(() => {
+      return testUtils.httpServer().start()
+    })
   }
 }
